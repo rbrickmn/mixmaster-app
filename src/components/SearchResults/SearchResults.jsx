d20fd403f2035bb356892c0c4d02e1ee49223478
rbrickmn <rbrickmn@gmail.com>
2024-06-01 18:48:12 -0400

    Create structure for playlist view

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -7,23 +7,71 @@ function SearchResults() {
       <div className={styles.SearchResultsWrapper}>
         <p className={styles.ResultsTitle}>Results for "Song Title":</p>
 
+        <hr />
+
         <div className={styles.Results}>
           <div className={styles.SongResult}>
-            <div className={styles.AlbumCover}></div>
-            <div className={styles.SongInfo}>
-              <p className={styles.SongTitle}>Song Title</p>
-              <p className={styles.SongArtist}>Artist</p>
+            <div className={styles.SongContainer}>
+              <div className={styles.AlbumCover}></div>
+              <div className={styles.SongInfo}>
+                <p className={styles.SongTitle}>Song Title</p>
+                <p className={styles.SongArtist}>Artist</p>
+              </div>
+            </div>
+
+            <div className={styles.ButtonContainer}>
+              <button className={styles.AddBtn}>
+                <img
+                  className={styles.AddIcon}
+                  src="src\assets\add.svg"
+                  alt="Add Song Button"
+                />
+              </button>
             </div>
           </div>
 
           <div className={styles.SongResult}>
-            <div className={styles.AlbumCover}></div>
-            <div className={styles.SongInfo}>
-              <p className={styles.SongTitle}>Song Title</p>
-              <p className={styles.SongArtist}>Artist</p>
+            <div className={styles.SongContainer}>
+              <div className={styles.AlbumCover}></div>
+              <div className={styles.SongInfo}>
+                <p className={styles.SongTitle}>Song Title</p>
+                <p className={styles.SongArtist}>Artist</p>
+              </div>
+            </div>
+
+            <div className={styles.ButtonContainer}>
+              <button className={styles.AddBtn}>
+                <img
+                  className={styles.AddIcon}
+                  src="src\assets\add.svg"
+                  alt="Add Song Button"
+                />
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.SongResult}>
+            <div className={styles.SongContainer}>
+              <div className={styles.AlbumCover}></div>
+              <div className={styles.SongInfo}>
+                <p className={styles.SongTitle}>Song Title</p>
+                <p className={styles.SongArtist}>Artist</p>
+              </div>
+            </div>
+
+            <div className={styles.ButtonContainer}>
+              <button className={styles.AddBtn}>
+                <img
+                  className={styles.AddIcon}
+                  src="src\assets\add.svg"
+                  alt="Add Song Button"
+                />
+              </button>
             </div>
           </div>
         </div>
+
+        <hr />
       </div>
     </>
   );
