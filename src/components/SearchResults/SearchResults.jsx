3fd2933954927e4571effdafe0a083c352f2cee6
rbrickmn <rbrickmn@gmail.com>
2024-06-03 10:28:52 -0400

    Add responsive design to SearchResults

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -29,46 +29,6 @@ function SearchResults() {
               </button>
             </div>
           </div>
-
-          <div className={styles.SongResult}>
-            <div className={styles.SongContainer}>
-              <div className={styles.AlbumCover}></div>
-              <div className={styles.SongInfo}>
-                <p className={styles.SongTitle}>Song Title</p>
-                <p className={styles.SongArtist}>Artist</p>
-              </div>
-            </div>
-
-            <div className={styles.ButtonContainer}>
-              <button className={`${styles.AddBtn} ${styles.Checked}`}>
-                <img
-                  className={styles.AddIcon}
-                  src="src\assets\check.svg"
-                  alt="Add Song Button"
-                />
-              </button>
-            </div>
-          </div>
-
-          <div className={styles.SongResult}>
-            <div className={styles.SongContainer}>
-              <div className={styles.AlbumCover}></div>
-              <div className={styles.SongInfo}>
-                <p className={styles.SongTitle}>Song Title</p>
-                <p className={styles.SongArtist}>Artist</p>
-              </div>
-            </div>
-
-            <div className={styles.ButtonContainer}>
-              <button className={styles.AddBtn}>
-                <img
-                  className={styles.AddIcon}
-                  src="src\assets\add.svg"
-                  alt="Add Song Button"
-                />
-              </button>
-            </div>
-          </div>
         </div>
 
         <hr />
